37da17c624450d99602302eb2ea50b736f9d7405
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-03-12 17:19:59 -0300

    feat: add auth token repository interface

---
+++ src/domain/main/app/repositories/auth-token-repository.ts
@@ -0,0 +1,9 @@
+import { AuthToken } from '@domain/main/enterprise/entities/auth-token'
+
+export abstract class TokenRepository {
+  abstract findByRefreshToken: (
+    refreshToken: string
+  ) => Promise<AuthToken | null>
+  abstract create: (token: AuthToken) => Promise<void>
+  abstract deleteByUserId: (userId: string) => Promise<void>
+}
