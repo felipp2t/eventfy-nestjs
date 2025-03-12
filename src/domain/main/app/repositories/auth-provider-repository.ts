ed00bcea2ff261a75612da0361fcbd572799ad0b
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-03-12 17:19:30 -0300

    feat: add auth provider repository interface

---
+++ src/domain/main/app/repositories/auth-provider-repository.ts
@@ -0,0 +1,11 @@
+import { AuthProvider } from '@domain/main/enterprise/entities/auth-provider'
+import { AUTH_PROVIDERS } from 'core/constants/auth-provider'
+
+export abstract class AuthProviderRepository {
+  abstract findById: (id: string) => Promise<AuthProvider | null>
+  abstract findByUserId: (userId: string) => Promise<AuthProvider[] | null>
+  abstract findByProvider: (
+    provider: AUTH_PROVIDERS
+  ) => Promise<AuthProvider[] | null>
+  abstract create: (provider: AuthProvider) => Promise<void>
+}
