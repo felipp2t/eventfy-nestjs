247862f72bdb84c5a25548d05e403dc473f8bb70
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-03-13 09:35:14 -0300

    feat: implement InMemoryAuthProviderRepository for managing auth providers in tests

---
+++ test/in-memory/in-memory-auth-provider-repository.ts
@@ -0,0 +1,23 @@
+import { AuthProviderRepository } from "@domain/main/app/repositories/auth-provider-repository"
+import { AuthProvider } from "@domain/main/enterprise/entities/auth-provider"
+import { AUTH_PROVIDERS } from "core/constants/auth-provider"
+
+export class InMemoryAuthProviderRepository implements AuthProviderRepository {
+  public items: AuthProvider[] = []
+
+  async findById(id: string) {
+    return this.items.find(provider => provider.id.toString() === id) || null
+  }
+
+  async findByUserId(userId: string) {
+    return this.items.filter(provider => provider.userId.toString() === userId)
+  }
+
+  async findByProvider(provider: AUTH_PROVIDERS) {
+    return this.items.filter(p => p.provider === provider) || null
+  }
+
+  async create(provider: AuthProvider) {
+    this.items.push(provider)
+  }
+}
