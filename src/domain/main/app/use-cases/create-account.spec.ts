05867162a3569f786c0f933f1cba9f8d7dedb7d6
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-03-13 09:42:02 -0300

    refactor: change folder location

--- src/domain/main/app/use-cases/create-account.spec.ts
+++ src/domain/main/app/use-cases/create-account.spec.ts
@@ -1,6 +1,6 @@
 import { EmailAlreadyInUse } from 'core/errors/errors/email-already-in-use'
-import { FakeEncrypter } from 'infra/cryptography/fake-encrypter'
-import { FakeHasher } from 'infra/cryptography/fake-hasher'
+import { FakeEncrypter } from '../../../../../test/cryptography/fake-encrypter'
+import { FakeHasher } from '../../../../../test/cryptography/fake-hasher'
 import { InMemoryAuthProviderRepository } from '../../../../../test/in-memory/in-memory-auth-provider-repository'
 import { InMemoryAuthTokenRepository } from '../../../../../test/in-memory/in-memory-auth-token-repository'
 import { InMemoryUserRepository } from '../../../../../test/in-memory/in-memory-user-repository'
