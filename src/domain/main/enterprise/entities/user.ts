7582556cfd4e202c7ce4023ecfcca23aba376c53
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-03-20 16:28:33 -0300

    refactor: add setter for password in User entity

--- src/domain/main/enterprise/entities/user.ts
+++ src/domain/main/enterprise/entities/user.ts
@@ -17,6 +17,10 @@ export class User extends Entity<UserProps> {
     return this.props.password
   }
 
+  set password(password: string) {
+    this.props.password = password
+  }
+
   get createdAt(): Date {
     return this.props.createdAt
   }
