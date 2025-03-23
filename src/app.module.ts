a0d9374ada495aa220ed825af9301cc02a13acde
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-03-23 18:26:19 -0300

    chore: configure global environment variables with validation in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,15 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './infra/env/env'
+import { EnvModule } from './infra/env/env.module'
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: env => envSchema.parse(env),
+    }),
+    EnvModule,
+  ],
+})
 export class AppModule {}
