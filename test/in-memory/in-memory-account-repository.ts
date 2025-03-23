bac5ba422d3808eec3134b656dbdbe86444eab06
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-03-23 20:44:26 -0300

    feat: refactor in-memory repositories, remove user repository and implement password update functionality

--- test/in-memory/in-memory-account-repository.ts
+++ test/in-memory/in-memory-account-repository.ts
@@ -1,43 +1,25 @@
 import {
   AccountRepository,
-  FindByProviderAndId,
-  FindByUserIdAndProvider,
+  UpdatePasswordParams,
 } from '@domain/main/app/repositories/account-repository'
 import { Account } from '@domain/main/enterprise/entities/account'
+import { Password } from '@domain/main/enterprise/entities/value-objects/password'
 
 export class InMemoryAccountRepository implements AccountRepository {
   public items: Account[] = []
 
   async findById(id: string): Promise<Account | null> {
-    return this.items.find(account => account.id.toString() === id) || null
+    return this.items.find(account => account.id.toString() === id) ?? null
   }
-
-  async findByUserId(userId: string): Promise<Account[] | null> {
-    return this.items.filter(account => account.userId.toString() === userId)
+  async findByEmail(email: string): Promise<Account | null> {
+    return this.items.find(account => account.email === email) ?? null
   }
-
-  async findByProviderAndId({
-    provider,
-    providerId,
-  }: FindByProviderAndId): Promise<Account | null> {
-    return (
-      this.items.find(
-        account =>
-          account.provider === provider && account.providerId === providerId
-      ) || null
-    )
-  }
-
-  async findByUserIdAndProvider({
-    userId,
-    provider,
-  }: FindByUserIdAndProvider): Promise<Account | null> {
-    return (
-      this.items.find(
-        account =>
-          account.userId.toString() === userId && account.provider === provider
-      ) || null
-    )
+  async updatePassword({
+    accountId,
+    password,
+  }: UpdatePasswordParams): Promise<void> {
+    const account = this.items.find(account => account.id.toString() === accountId)
+    account!.password = await Password.create(password)
   }
 
   async create(account: Account): Promise<void> {
