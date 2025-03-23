2d148eb7e040c6e7bbc60ed34bc21869dde155c7
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-03-23 18:24:40 -0300

    chore: add setup Vitest configuration for end-to-end testing

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,20 @@
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+    setupFiles: ['./test/setup-e2e.ts'],
+    hookTimeout: 30000,
+    testTimeout: 60000,
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+})
