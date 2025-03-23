76d606752baa075a5a9dddd9b3d6379012fa5bd7
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-03-23 18:26:53 -0300

    chore: enhance bootstrap function with global prefix and dynamic port configuration

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../app.module';
+import { NestFactory } from '@nestjs/core'
+import { EnvService } from './env/env.service'
+import { AppModule } from 'src/app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error'],
+  })
+
+  app.setGlobalPrefix('api')
+
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
+
+  await app.listen(port)
 }
-bootstrap();
+bootstrap()
