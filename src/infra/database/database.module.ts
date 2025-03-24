08e0fb9d1e28ce9ec44927582a1efc94a8a18fdd
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-03-24 09:46:48 -0300

    feat: add SessionRepository to DatabaseModule for session management

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,13 +1,16 @@
 import { AccountRepository } from '@domain/main/app/repositories/account-repository'
+import { SessionRepository } from '@domain/main/app/repositories/session-repository'
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { PrismaAccountRepository } from './prisma/repositories/prisma-account-repository'
+import { PrismaSessionRepository } from './prisma/repositories/prisma-session-repository'
 
 @Module({
   providers: [
     PrismaService,
     { provide: AccountRepository, useClass: PrismaAccountRepository },
+    { provide: SessionRepository, useClass: PrismaSessionRepository },
   ],
-  exports: [PrismaService, AccountRepository],
+  exports: [PrismaService, AccountRepository, SessionRepository],
 })
 export class DatabaseModule {}
