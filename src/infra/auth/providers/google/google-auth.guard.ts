4dd3e31a90496b39ccaa348c037993ad58dd1950
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-03-25 10:46:14 -0300

    feat: add Google authentication support with strategy and guards

--- src/infra/auth/providers/google/google-auth.guard.ts
+++ src/infra/auth/providers/google/google-auth.guard.ts
@@ -1,24 +1,19 @@
+import { IS_PUBLIC_KEY } from '@infra/auth/public'
 import { ExecutionContext, Injectable } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { AuthGuard } from '@nestjs/passport'
-import { IS_PUBLIC_KEY } from './public.js'
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
+export class GoogleAuthGuard extends AuthGuard('google') {
   constructor(private reflector: Reflector) {
     super()
   }
 
   canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest()
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ])
-
-    if (request.path.startsWith('/api/auth/google')) {
-      return new (AuthGuard('google'))().canActivate(context)
-    }
 
     if (isPublic) {
       return true
